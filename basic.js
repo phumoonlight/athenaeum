5b753a8cdacdb757c17fab7024d91c7d5588f826
Phusan Moonmuang <phumoonlight@hotmail.com>
2020-07-21 15:04:29 +0700

    remove extra line

--- basic.js
+++ basic.js
@@ -4,7 +4,6 @@ let y = 2
 const z = 3
 
 // declare functions
-
 function doSomething(a, b) {
   return a + b
 }
@@ -12,12 +11,10 @@ function doSomething(a, b) {
 const doSomethingArrow = (a, b) => a + b
 
 // using functions
-
 const result1 = doSomething(1, 2)
 const result2 = doSomethingArrow(1, 2)
 
 // conditional
-
 const condition = true
 if (condition) {
   // ...do something when true
@@ -28,7 +25,6 @@ if (condition) {
 const resultShortIf = condition ? doSomething(1, 2) : null
 
 // object
-
 const person = {
   name: 'com',
   age: 99,
@@ -40,9 +36,6 @@ const person = {
 }
 
 // template string
-
 const age = 44
-
 const textTemplate = `คุณอายุ ${example}`
-
 const textString = 'คุณอายุ ${example}'
