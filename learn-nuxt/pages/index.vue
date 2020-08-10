801bbd24c55f1727a4b349e97a9e39bb1fcdd011
Phusan Moonmuang <phumoonlight@hotmail.com>
2020-08-10 23:22:32 +0700

    updatepackage

--- learn-nuxt/pages/index.vue
+++ learn-nuxt/pages/index.vue
@@ -1,5 +1,22 @@
 <template>
   <div>
-    Hello World!
+    Hello World! {{ name }}
+    <el-button type="primary">Primary</el-button>
   </div>
 </template>
+
+<script>
+import { Button } from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+export default {
+  asyncData() {
+    return {
+      name: 'phumo'
+    }
+  },
+  components: {
+    [Button.name]: Button
+  }
+}
+</script>
