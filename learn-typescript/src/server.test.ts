82f1000311354925ac4d1d499e034c3a90046899
Phusan Moonmuang <phumoonlight@hotmail.com>
2020-09-24 23:51:06 +0700

    update

--- learn-typescript/src/server.test.ts
+++ learn-typescript/src/server.test.ts
@@ -1,10 +1,11 @@
+import {someModule} from './example-module'
 
 describe("A suite is just a function", function() {
   var a;
 
   it("and so is a spec", function() {
     a = true;
-
+    someModule()
     expect(a).toBe(true);
   });
 });
