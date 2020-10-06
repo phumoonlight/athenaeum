b61daa6d801370ef2412940d01f49cd3e787ab86
phumoonlight <phumoonlight@hotmail.com>
2020-10-06 12:11:26 +0700

    update go

---
+++ golang/basic/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("main")
+	helloworld()
+	constant()
+	variable()
+	sandbox()
+}
---
+++ golang/basic/sandbox.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func sandbox() {
+	fmt.Println("sandbox")
+}
