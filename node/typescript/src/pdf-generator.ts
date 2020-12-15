bd51dde7aead4eaa4aff894b55880af61219fd26
poosarn <phusan.official@gmail.com>
2020-12-15 10:49:30 +0700

    Update pdf-generator.ts

--- node/typescript/src/pdf-generator.ts
+++ node/typescript/src/pdf-generator.ts
@@ -1,13 +1,16 @@
 import puppeteer from 'puppeteer'
 
-export class PdfGenerator {
+class PdfGenerator {
   async testGenerate() {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto('https://phumoonlight.vercel.app/profile', {
       waitUntil: ['load', 'networkidle0', 'domcontentloaded', 'networkidle2']
     });
-    await page.pdf({ path: './output/test.pdf', format: 'A4' });
+    await page.pdf({ path: 'output/test.pdf', format: 'A4' });
     await browser.close();
   }
 }
+
+const generator = new PdfGenerator()
+generator.testGenerate()
