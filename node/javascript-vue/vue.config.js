9eea6e952599bad1e22e41061c03eb58c845079f
phumoonlight <phumoonlight@hotmail.com>
2020-12-15 17:39:23 +0700

    Create vue.config.js

---
+++ node/javascript-vue/vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      title: 'Vue | Athenaeum',
+    },
+    about: {
+      entry: 'src/main.js',
+      title: 'About | Vue | Athenaeum',
+    },
+  }
+}
