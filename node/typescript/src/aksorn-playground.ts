ba37bdc08db8568b71cc0c058dc8ac6e852cd65a
phumoonlight <phumoonlight@hotmail.com>
2021-01-04 16:21:44 +0700

    Update aksorn-playground.ts

--- node/typescript/src/aksorn-playground.ts
+++ node/typescript/src/aksorn-playground.ts
@@ -2,8 +2,8 @@ import Axios from 'axios'
 
 const apiKey = process.argv[2]
 const hardCodedSearchSchool = async (startPage: number, schoolCode: string) => {
-  console.info(new Date() ,'current-page:', startPage)
-  const response =  await Axios.get(`https://backoffice-api.aksorn-dev.mydevkit.me/api/schools?pageNumber=${startPage}&pageSize=100`, {
+  console.info(new Date(), 'current-page:', startPage)
+  const response = await Axios.get(`https://backoffice-api.aksorn-dev.mydevkit.me/api/schools?pageNumber=${startPage}&pageSize=100`, {
     headers: {
       'X-API-KEY': apiKey
     }
