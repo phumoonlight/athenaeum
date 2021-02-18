e4097c9805bfd6cce833b2c19b459b50753c2b34
Poosarn Moolmuang <phumoonlight@hotmail.com>
2021-02-18 11:31:48 +0700

    remove workshop project

--- workshop/measure-station/src/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import 'antd/dist/antd.css'
-import '../styles/global.css'
-import { AppProps } from 'next/app'
-
-const App: React.FC<AppProps> = ({ Component, pageProps }) => (
-  <Component {...pageProps} />
-)
-
-export default App
