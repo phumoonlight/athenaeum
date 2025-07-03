14dd25065f2cbd9f1aad1ce7c71f455dff5262db
phumoonlight <phumoonlight@hotmail.com>
2025-07-03 15:35:19 +0700

    move

--- node/_workshop/cms/src/main.ts
+++ node/_workshop/cms/src/main.ts
@@ -2,8 +2,11 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
 const bootstrap = async () => {
+  const port = process.env.PORT || 3000
   const app = await NestFactory.create(AppModule)
-  await app.listen(process.env.PORT ?? 3000)
+  await app.listen(port, () => {
+    console.log(`Application is running on: http://localhost:${port}`)
+  })
 }
 
 void bootstrap()
