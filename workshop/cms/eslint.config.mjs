48e9f27bd959629dde90ae87764d7275d7d40ea5
phumoonlight <phumoonlight@hotmail.com>
2025-07-03 15:19:38 +0700

    workshop cms

--- workshop/cms/eslint.config.mjs
+++ workshop/cms/eslint.config.mjs
@@ -1,16 +1,11 @@
-// @ts-check
-import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import eslint from '@eslint/js'
+import stylistic from '@stylistic/eslint-plugin'
 
 export default tseslint.config(
-  {
-    ignores: ['eslint.config.mjs'],
-  },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
   {
     languageOptions: {
       globals: {
@@ -25,10 +20,34 @@ export default tseslint.config(
     },
   },
   {
+    plugins: {
+      '@stylistic': stylistic
+    },
     rules: {
+      'no-unused-vars': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      '@typescript-eslint/no-unused-vars': 'error',
+      '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+      '@stylistic/quotes': ['error', 'single'],
+      '@stylistic/indent': ['error', 2],
+      '@stylistic/semi': ['error', 'never'],
+      '@stylistic/key-spacing': 'error',
+      '@stylistic/keyword-spacing': 'error',
+      '@stylistic/space-infix-ops': 'error',
+      '@stylistic/function-call-spacing': ['error', 'never'],
+      '@stylistic/arrow-parens': ['error', 'always'],
+      '@stylistic/member-delimiter-style': ['error', {
+        'multiline': {
+          'delimiter': 'none',
+        },
+        'singleline': {
+          'delimiter': 'semi',
+        },
+        'multilineDetection': 'brackets'
+      }]
     },
   },
 );
+
